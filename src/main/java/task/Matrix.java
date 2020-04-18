6a73961753a4da6441868b270e68ff0aa0b00d29
Arseniy Obolenskiy <gooddoog@student.su>
2020-04-18 23:50:37 +0300

    Add addition and subtraction operations for matrices and tests for them

--- src/main/java/task/Matrix.java
+++ src/main/java/task/Matrix.java
@@ -1,6 +1,26 @@
 package task;
 
 public class Matrix {
+    public static double[][] add(double[][] a, double[][] b) {
+        double[][] c = new double[a.length][a[0].length];
+        for (int row = 0; row < c.length; ++row) {
+            for (int col = 0; col < c.length; ++col) {
+                c[row][col] = a[row][col] + b[row][col];
+            }
+        }
+        return c;
+    }
+
+    public static double[][] subtract(double[][] a, double[][] b) {
+        double[][] c = new double[a.length][a[0].length];
+        for (int row = 0; row < c.length; ++row) {
+            for (int col = 0; col < c.length; ++col) {
+                c[row][col] = a[row][col] - b[row][col];
+            }
+        }
+        return c;
+    }
+
     public static double[][] multiply(double[][] a, double[][] b) {
         double[][] c = new double[a.length][b[0].length];
         for (int row = 0; row < c.length; row++) {
--- src/test/java/task/MatrixTest.java
+++ src/test/java/task/MatrixTest.java
@@ -2,6 +2,7 @@ package task;
 
 import java.util.Arrays;
 import org.junit.Test;
+import org.junit.Assert;
 
 public class MatrixTest {
     @Test
@@ -19,6 +20,42 @@ public class MatrixTest {
             {15, 22}
         };
         double[][] answer = Matrix.multiply(a, b);
-        Arrays.deepEquals(expected, answer);
+        Assert.assertTrue(Arrays.deepEquals(expected, answer));
+    }
+
+    @Test
+    public void simpleAdditionTest() {
+        double[][] a = {
+            {1, 2},
+            {3, 4}
+        };
+        double[][] b = {
+            {1, 2},
+            {3, 4}
+        };
+        double[][] expected = {
+            {2, 4},
+            {6, 8}
+        };
+        double[][] answer = Matrix.add(a, b);
+        Assert.assertTrue(Arrays.deepEquals(expected, answer));
+    }
+
+    @Test
+    public void simpleSubtractionTest() {
+        double[][] a = {
+            {1, 2},
+            {3, 4}
+        };
+        double[][] b = {
+            {1, 2},
+            {3, 4}
+        };
+        double[][] expected = {
+            {0, 0},
+            {0, 0}
+        };
+        double[][] answer = Matrix.subtract(a, b);
+        Assert.assertTrue(Arrays.deepEquals(expected, answer));
     }
 }
